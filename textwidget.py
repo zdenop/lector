80052af2ecf7d3ed02442216202cc205a5f2422d
davide.setti <davide.setti@localhost>
2008-03-09 14:37:43 +0000

    Esporta il testo in RTF via abiword

--- textwidget.py
+++ textwidget.py
@@ -18,4 +18,17 @@ class TextWidget(QtGui.QTextBrowser):
 
 
     def saveAs(self, filename):
+        import os, codecs
+
         print filename
+        fileTmp = '/tmp/prova.html'
+#        s = unicode(self.toHtml().toUtf8(), 'utf8')
+
+#        fd = codecs.open(fileTmp, 'w','utf-8')
+        s = self.document().toHtml('utf-8').toUtf8()
+        fd = open(fileTmp, 'w')
+        fd.write(s)
+        fd.close()
+
+        cmd = "abiword --to=rtf %s" % (fileTmp, )
+        os.popen(cmd)
