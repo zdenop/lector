48ee25c37e851d40e456bfaa3859db6c6e28083b
davide.setti <davide.setti@localhost>
2008-05-12 17:22:55 +0000

    Removed some old tranlations

--- ts/lector_it_IT.ts
+++ ts/lector_it_IT.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="it">
-<defaultcodec></defaultcodec>
 <context>
     <name>Lector</name>
     <message>
@@ -19,37 +18,37 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="216"/>
+        <location filename="ui_lector.ui" line="188"/>
         <source>Exit</source>
         <translation>Esci</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="224"/>
+        <location filename="ui_lector.ui" line="196"/>
         <source>Rotate right</source>
         <translation>Ruota a destra</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="232"/>
+        <location filename="ui_lector.ui" line="204"/>
         <source>Rotate left</source>
         <translation>Ruota a sinistra</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="240"/>
+        <location filename="ui_lector.ui" line="212"/>
         <source>Flip</source>
         <translation>Ribalta</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="248"/>
+        <location filename="ui_lector.ui" line="220"/>
         <source>Zoom in</source>
         <translation>Ingrandisci</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="259"/>
+        <location filename="ui_lector.ui" line="231"/>
         <source>Zoom out</source>
         <translation>Rimpicciolisci</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="270"/>
+        <location filename="ui_lector.ui" line="242"/>
         <source>Read</source>
         <translation>Leggi</translation>
     </message>
@@ -59,32 +58,17 @@
         <translation>Lingua</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="145"/>
-        <source>Italian</source>
-        <translation>Italiano</translation>
-    </message>
-    <message>
-        <location filename="ui_lector.ui" line="138"/>
-        <source>German</source>
-        <translation>Tedesco</translation>
-    </message>
-    <message>
-        <location filename="ui_lector.ui" line="128"/>
-        <source>English</source>
-        <translation>Inglese</translation>
-    </message>
-    <message>
-        <location filename="ui_lector.ui" line="167"/>
+        <location filename="ui_lector.ui" line="139"/>
         <source>Text</source>
         <translation>Testo</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="205"/>
+        <location filename="ui_lector.ui" line="177"/>
         <source>&amp;Open image</source>
         <translation>&amp;Apri immagine</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="208"/>
+        <location filename="ui_lector.ui" line="180"/>
         <source>Ctrl+O</source>
         <translation>Ctrl+O</translation>
     </message>
@@ -94,32 +78,32 @@
         <translation>Visualizza</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="251"/>
+        <location filename="ui_lector.ui" line="223"/>
         <source>Ctrl++</source>
         <translation>Ctrl++</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="262"/>
+        <location filename="ui_lector.ui" line="234"/>
         <source>Ctrl+-</source>
         <translation>Ctrl+-</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="278"/>
+        <location filename="ui_lector.ui" line="250"/>
         <source>Save &amp;as</source>
         <translation>Sa&amp;lva con nome</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="158"/>
+        <location filename="ui_lector.ui" line="130"/>
         <source>New area</source>
         <translation>Nuova area</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="177"/>
+        <location filename="ui_lector.ui" line="149"/>
         <source>Image</source>
         <translation>Immagine</translation>
     </message>
     <message>
-        <location filename="ui_lector.ui" line="281"/>
+        <location filename="ui_lector.ui" line="253"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
@@ -150,42 +134,42 @@
         <translation>Pronto</translation>
     </message>
     <message>
-        <location filename="lector.py" line="81"/>
+        <location filename="lector.py" line="80"/>
         <source>Open image</source>
         <translation>Apri immagine</translation>
     </message>
     <message>
-        <location filename="lector.py" line="83"/>
+        <location filename="lector.py" line="82"/>
         <source>Images (*.png *.xpm *.jpg)</source>
         <translation>Immagini (*.png *.xpm *.jpg)</translation>
     </message>
     <message>
-        <location filename="lector.py" line="171"/>
+        <location filename="lector.py" line="157"/>
         <source>Are you sure you want to exit?</source>
         <translation>Sei sicuro di voler uscire?</translation>
     </message>
     <message>
-        <location filename="lector.py" line="180"/>
+        <location filename="lector.py" line="166"/>
         <source>Save document</source>
         <translation>Salva documento</translation>
     </message>
     <message>
-        <location filename="lector.py" line="181"/>
+        <location filename="lector.py" line="167"/>
         <source>RTF document</source>
         <translation>Documento RTF</translation>
     </message>
     <message>
-        <location filename="lector.py" line="49"/>
+        <location filename="lector.py" line="46"/>
         <source>English</source>
         <translation>Inglese</translation>
     </message>
     <message>
-        <location filename="lector.py" line="50"/>
+        <location filename="lector.py" line="47"/>
         <source>Italian</source>
         <translation>Italiano</translation>
     </message>
     <message>
-        <location filename="lector.py" line="51"/>
+        <location filename="lector.py" line="48"/>
         <source>German</source>
         <translation>Tedesco</translation>
     </message>
