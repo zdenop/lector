45cb2af0b9cc9b08be84ab64b7514c2d28790a4c
davide.setti <davide.setti@localhost>
2011-02-01 22:52:23 +0000

    updating translations

--- ts/lector_de_DE.ts
+++ ts/lector_de_DE.ts
@@ -450,7 +450,7 @@
     </message>
     <message>
         <location filename="lector.py" line="284"/>
-        <source>ODF document</source>
+        <source>ODT document</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
