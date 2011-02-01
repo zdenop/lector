4d7f990e3a6b1bb389fe0b126ae2abe07fd1dfb0
davide.setti <devnull@localhost>
2011-02-01 22:39:05 +0000

    removed abiword dependency, Lector saves ODF now

--- textwidget.py
+++ textwidget.py
@@ -2,7 +2,7 @@
 
 """ Lector: textwidget.py
 
-    Copyright (C) 2008 Davide Setti
+    Copyright (C) 2011 Davide Setti
 
     This program is released under the GNU GPLv2
 """
@@ -18,18 +18,8 @@ class TextWidget(QtGui.QTextBrowser):
         self.setReadOnly(False)
 
     def saveAs(self, filename):
-        from os import popen
-        from shutil import move
-
-        ## TODO: replace with Qt's temp files
-        fileTmp = '/tmp/prova.html'
-        s = self.document().toHtml('utf-8').toUtf8()
-        with open(fileTmp, 'w') as fd:
-            fd.write(s)
-
-        cmd = "abiword --to=rtf %s" % (fileTmp, )
-        popen(cmd)
-
-        fileTmpRtf = fileTmp.split('.')[0] + '.rtf'
-        move(fileTmpRtf, filename)
+        dw = QtGui.QTextDocumentWriter()
+        dw.setFormat('ODF')
+        dw.setFileName(filename)
+        dw.write(self.document())
 
