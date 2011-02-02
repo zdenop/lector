dc93fc0eae41174cedecda01e5306c300008223e
davide.setti <devnull@localhost>
2011-02-02 19:40:22 +0000

    Checkbox in settings to choose Color, Gray or Lineart. Gray and Lineart have problems with python-imaging-sane (replace it with scanimage)

--- settingsdialog.py
+++ settingsdialog.py
@@ -4,6 +4,8 @@ from ui.ui_settings import Ui_Settings
 from utils import settings
 
 class Settings(QDialog):
+    colors = ['Color', 'Gray', 'Lineart']
+
     def __init__(self, parent = None):
         QDialog.__init__(self, parent)
 
@@ -13,11 +15,17 @@ class Settings(QDialog):
         self.ui.sbHeight.setValue(settings.get('scanner:height'))
         self.ui.sbWidth.setValue(settings.get('scanner:width'))
         self.ui.sbResolution.setValue(settings.get('scanner:resolution'))
+        self.ui.combColor.setCurrentIndex(
+            self.colors.index(settings.get('scanner:mode'))
+        )
+
 
     def accept(self):
         settings.set('scanner:height', self.ui.sbHeight.value())
         settings.set('scanner:width', self.ui.sbWidth.value())
         settings.set('scanner:resolution', self.ui.sbResolution.value())
+        settings.set('scanner:mode',
+                     self.colors[self.ui.combColor.currentIndex()])
 
         QDialog.accept(self)
 
