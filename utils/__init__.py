0018d124510ae0cd56f2bc990f2dcf2a77e9b710
davide.setti <davide.setti@localhost>
2011-02-02 10:34:34 +0000

    refactoring tesseract languages detection

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,9 @@
 from PyQt4.QtGui import QImage
 from PyQt4.QtCore import QByteArray
+from subprocess import Popen, PIPE
+from glob import glob
+import os
+
 
 def pilImage2Qt(im):
     ## TODO: check if it's necessary to convert to RGB (maybe only for grayscale images)
@@ -11,3 +15,28 @@ def pilImage2Qt(im):
 
     #self.ocrImage = ImageQt.ImageQt(self.im.convert("RGB"))
 
+
+def extract_tesseract_languages_path(error_message):
+    """
+    >>> extract_tesseract_languages_path("Unable to load unicharset file /usr/share/tesseract-ocr/tessdata/invalid.unicharset")
+    ('/usr/share/tesseract-ocr/tessdata', '.unicharset')
+    """
+    invalid_path = error_message.split()[-1]
+    path, invalid_fn = os.path.split(invalid_path)
+    _, extension = os.path.splitext(invalid_fn)
+    return path, extension
+
+
+def get_tesseract_languages():
+    ## TODO: check if tesseract is installed
+    if os.getenv('TESSDATA_PREFIX') is None:
+        poTess = Popen('tesseract try try -l invalid', stderr=PIPE,
+        shell=True)
+        lTess = poTess.stderr.readline()
+        tessdata_path, langdata_ext = extract_tesseract_languages_path(lTess)
+    else:
+        tessdata_path = os.path.join(os.getenv('TESSDATA_PREFIX'), "tessdata")
+        langdata_ext = '.unicharset'
+
+    langdata = glob(tessdata_path + os.path.sep + '*' + langdata_ext)
+    return [os.path.splitext(os.path.split(uc)[1])[0] for uc in langdata]
